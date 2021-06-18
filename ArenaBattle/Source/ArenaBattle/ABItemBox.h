498d05ea983e3a97d68317f0514d6243b8535096
JO43425 <71028060+JO43425@users.noreply.github.com>
2021-06-18 14:01:05 +0900

    Added ItemBox, Weapon

---
+++ ArenaBattle/Source/ArenaBattle/ABItemBox.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ArenaBattle.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ABItemBox.generated.h"
+
+UCLASS()
+class ARENABATTLE_API AABItemBox : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AABItemBox();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void PostInitializeComponents() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(VisibleAnyWhere, Category = Box)
+		UBoxComponent* Trigger;
+
+	UPROPERTY(VisibleAnyWhere, Category = Box)
+		UStaticMeshComponent* Box;
+
+	UPROPERTY(EditInstanceOnly, Category = Box)
+		TSubclassOf<class AABWeapon> WeaponItemClass;
+
+	UPROPERTY(VisibleAnywhere, Category = Effect)
+		UParticleSystemComponent* Effect;
+	
+protected:
+	UFUNCTION()
+		void OnChacterOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+			const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnEffectFinished(class UParticleSystemComponent* PSystem);
+};
